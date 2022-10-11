700383549b71f2d9f5f4ae53ebe70e9ad4b813e5
Kanem <kanemdeng@email.com>
2022-10-11 11:45:28 -0700

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  jsxBracketSameLine: false,
+  bracketSameLine: false,
+  singleAttributePerLine: false
+};
